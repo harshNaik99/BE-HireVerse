6289e039f12e6a00fdd5d57a491cccd37d686d0f
raj <rajshah97120@gmail.com>
2025-12-16 09:53:29 -0800

    fix: add dotenv dependency

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import connectDB from './db/db_connection.js';
 import app from './configs/express.config.js'
 
 connectDB()
+
 .then(()=>{
     app.listen(process.env.PORT || 3000, ()=>{
         console.log("Server is running.........")
